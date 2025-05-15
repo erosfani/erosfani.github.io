32fb4c78dcef8fc2b586441ee4223f04bac0c489
Eros Fanì <eros.fani@gmail.com>
2025-05-15 17:57:46 +0200

    updated vite.config.js to keep teasers folder when deploying

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,21 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { viteStaticCopy } from 'vite-plugin-static-copy'
 
 // https://vite.dev/config/
 export default defineConfig({
   base: '/',
-  plugins: [react()],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: 'src/assets/teasers/*',
+          dest: 'assets/teasers',
+        },
+      ],
+    }),
+  ],
   define: {
     __BUILD_DATE__: JSON.stringify(new Date().toISOString()),
   },
