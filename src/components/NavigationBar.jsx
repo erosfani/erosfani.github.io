8b9eb1448bb052db973205de2c3a7154e51750b6
Eros Fanì <eros.fani@gmail.com>
2025-05-17 16:48:41 +0200

    automatically closing the menu for small screens after click

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,16 +1,21 @@
 import {Container, Nav, Navbar} from "react-bootstrap";
 import Dragon from "./logos/Dragon.jsx";
+import {useState} from "react";
 
 function NavigationBar(props) {
 
+  const [expanded, setExpanded] = useState(false);
+
   return (
 
-    <Navbar expand="sm" bg="light" variant="light" fixed="top">
+    <Navbar expand="sm" bg="light" variant="light" fixed="top"
+            expanded={expanded} onToggle={() => setExpanded(prev => !prev)}>
 
       <Container className='mx-auto'>
 
         <Navbar.Brand href="#about" active=""
-                      className={props.activeSection === 'about' ? 'active_tab' : undefined}>
+                      className={props.activeSection === 'about' ? 'active_tab' : undefined}
+                      onClick={() => setExpanded(false)}>
 
           <span className="logo-container">
             <Dragon />
@@ -26,15 +31,18 @@ function NavigationBar(props) {
 
           <Nav className="me-auto">
             <Nav.Link href="#experience" active=""
-                      className={props.activeSection === 'experience' ? 'active_tab' : undefined}>
+                      className={props.activeSection === 'experience' ? 'active_tab' : undefined}
+                      onClick={() => setExpanded(false)}>
               Experience
             </Nav.Link>
             <Nav.Link href="#education" active=""
-                      className={props.activeSection === 'education' ? 'active_tab' : undefined}>
+                      className={props.activeSection === 'education' ? 'active_tab' : undefined}
+                      onClick={() => setExpanded(false)}>
               Education
             </Nav.Link>
             <Nav.Link href="#publications" active=""
-                      className={props.activeSection === 'publications' ? 'active_tab' : undefined}>
+                      className={props.activeSection === 'publications' ? 'active_tab' : undefined}
+                      onClick={() => setExpanded(false)}>
               Publications
             </Nav.Link>
           </Nav>
